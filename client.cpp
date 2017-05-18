4dc12f2c7b1e8f5e3bbe5ff7ccf486a3abb669c1
DESKTOP-0L4C4ER\master <gitsoojin7897@naver.com>
2017-05-18 21:44:42 +0900

    #8 client <pull request>

--- client.cpp
+++ client.cpp
@@ -1,11 +1,30 @@
-#include "UdpClientSocket.h"
+//#include "UdpClientSocket.h"
+#include "TcpClientSocket.h"
 
 #define PORT 7082
 #define SERV_PORT 7080
 
 int main()
 {
+	char* message;
 	char IP[100], buf[BUFSIZE], file_name[100];
+
+	strcpy(IP, "127.0.0.1");
+	TcpClientSocket tcp(PORT, IP, SERV_PORT);
+	tcp.createSocket();
+	tcp.connectSocket();
+
+	strcpy(buf, "I want to download a file");
+	tcp.sendMessage(buf);
+
+	strcpy(buf, tcp.receiveMessage());
+	strcpy(file_name, buf);
+
+	tcp.receiveFile(file_name);
+
+	return 0;
+
+	/*char IP[100], buf[BUFSIZE], file_name[100];
 	IN_ADDR addr;
 
 	strcpy(IP, "127.0.0.1");
@@ -20,5 +39,5 @@ int main()
 
 	udp.receiveFile(file_name);
 
-	return 0;
+	return 0;*/
 }
