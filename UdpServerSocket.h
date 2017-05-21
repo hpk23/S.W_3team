4feef8570eb1fac8a281ec4830cfde82aebee9ae
hpk23 <hpk23@naver.com>
2017-05-21 22:00:57 +0900

    UDP에도 md5를 이용한 무결성 체크 추가 (#10)

--- UdpServerSocket.h
+++ UdpServerSocket.h
@@ -3,9 +3,15 @@
 #include <sys/types.h>
 #include <winsock2.h>
 #include <fstream>
-
-#define BUFSIZE 1024*24
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include "md5.h"
 #pragma comment(lib, "ws2_32.lib")
+
+#define BUFSIZE 1024 * 30
+#define LISTENNUM 5
 
 using namespace std;
 
@@ -29,6 +35,7 @@ public :
 	void createSocket();
 	void bindSocket();
 	void sendMessage(char* message);
+	string getHash(string md5Str);
 	void sendFile(char* file_name);
 	char* receiveMessage();
 };
@@ -82,21 +89,57 @@ void UdpServerSocket::sendMessage(char* message)
 	int mLen = strlen(message);
 	strcpy(buf, message);
 	sendto(servSock, buf, mLen, 0, (struct sockaddr *)&cliAddr, sizeof(cliAddr));
+	Sleep(10);
+}
+
+string UdpServerSocket::getHash(string md5Str)
+{
+	md5_state_t state;
+	md5_byte_t digest[16];
+	char hex_output[16*2+1];
+
+	md5_init(&state);
+	md5_append(&state, (const md5_byte_t *)md5Str.c_str(), md5Str.length());
+	md5_finish(&state, digest);
+	for(int i=0; i<16; i++)
+		sprintf(hex_output+i*2, "%02x", digest[i]);
+
+	return hex_output;
 }
 
 void UdpServerSocket::sendFile(char* file_name)
 {
-	ifstream file(file_name);
+	printf("UDP protocol\n\n");
+	string hash_value = "";
+	int len;
+	FILE *file;
 
-	if(file.is_open())
+
+	if( (file = fopen(file_name, "rb")) == NULL)
 	{
-		while(file.getline(buf, BUFSIZE))
-		{
-			strcat(buf, "\n");
-			sendMessage(buf);
-		}
-		file.close();
+		perror("fopen : ");
+		exit(1);
 	}
+
+	while( (len = fread(buf, 1, BUFSIZE, file)) )
+	{
+		buf[len] = 0;
+		hash_value = getHash(hash_value + buf);
+		sendMessage(buf);
+	}
+
+	// End Of File
 	strcpy(buf, "EOF");
 	sendMessage(buf);
+
+	//send file size
+	fseek(file, 0, SEEK_END);
+	int file_size = ftell(file);
+	sprintf(buf, "%d", file_size);
+	sendMessage(buf);
+	fclose(file);
+
+	//send hash_value
+	char* hash = (char*)hash_value.c_str();
+	sendMessage(hash);
 }
