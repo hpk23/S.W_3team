7becc4e34be925d5e8c10c65cb8d85f287f6110b
sunmin <sm20131398700@gmail.com>
2017-05-21 22:55:08 +0900

    #10 UdpClientSocket

--- UdpClientSocket.h
+++ UdpClientSocket.h
@@ -3,22 +3,26 @@
 #include <sys/types.h>
 #include <winsock2.h>
 #include <fstream>
+#include <windows.h>
+#include <iostream>
+#include <dirent.h>
+#include "md5.h"
 #pragma comment(lib, "ws2_32.lib")
 
-#define BUFSIZE 1024*24
+#define BUFSIZE 1024 * 30
 
 using namespace std;
 
 class UdpClientSocket
 {
-private :
+private:
 	char servIp[50];
-	char buf[BUFSIZE + 1];
-	
+	char buf[BUFSIZE + 5];
+
 	int port;
 	int servPort;
 	int peerLen;
-	
+
 	WSADATA wsaData;
 
 	sockaddr_in servAddr;
@@ -26,12 +30,13 @@ private :
 
 	SOCKET sock;
 
-public :
+public:
 	UdpClientSocket(int port, char* ip, int serv_port);
 	void createSocket();
 	void sendMessage(char* message);
 	void receiveFile(char* file_name);
 	char* receiveMessage();
+	string getHash(string md5Str);
 };
 
 UdpClientSocket::UdpClientSocket(int port, char* ip, int servPort)
@@ -74,33 +79,88 @@ char* UdpClientSocket::receiveMessage()
 {
 	int mLen = recvfrom(sock, buf, BUFSIZE, 0, (SOCKADDR *)&peerAddr, &peerLen);
 
-	if(memcmp(&peerAddr, &servAddr, sizeof(peerAddr)))
+	if (memcmp(&peerAddr, &servAddr, sizeof(peerAddr)))
 	{
-		printf("It is not dat afrom server..\n");
+		printf("서버로 부터 받은 데이터가 아닙니다.\n");
 		exit(1);
 	}
 	buf[mLen] = 0;
 	return buf;
 }
 
+string UdpClientSocket::getHash(string md5Str)
+{
+	md5_state_t state;
+	md5_byte_t digest[16];
+	char hex_output[16 * 2 + 1];
+
+	md5_init(&state);
+	md5_append(&state, (const md5_byte_t *)md5Str.c_str(), md5Str.length());
+	md5_finish(&state, digest);
+	for (int i = 0; i<16; i++)
+		sprintf(hex_output + i * 2, "%02x", digest[i]);
+
+	return hex_output;
+}
+
 void UdpClientSocket::receiveFile(char* file_name)
 {
-	ofstream outFile(file_name);
+	printf("UDP protocol\n\n");
+
+	FILE* outFile;
+
+	if ((outFile = fopen(file_name, "wb")) == NULL)
+	{
+		perror("outFile open : ");
+		exit(1);
+	}
 
 	strcpy(buf, receiveMessage());
-	while(strcmp(buf, "EOF"))
+	while (strcmp(buf, "EOF"))
 	{
-		if(outFile.is_open())
-		{
-			outFile << buf;
-			printf("%s", buf);
-			strcpy(buf, receiveMessage());
-		}
-		else
-		{
-			perror("outFile : ");
-			exit(1);
-		}
+		fwrite(buf, sizeof(buf[0]), strlen(buf), outFile);
+		strcpy(buf, receiveMessage());
 	}
-	outFile.close();
+	fclose(outFile);
+
+	string receive_hash_value;
+
+	// receive file size
+	string receive_file_size = receiveMessage();
+
+	// receive hash value
+	receive_hash_value = receiveMessage();
+
+	// get my hash value
+	FILE* myFile;
+	int len;
+
+	string my_hash_value = "";
+
+	if ((myFile = fopen(file_name, "rb")) == NULL)
+	{
+		perror("myfile open : ");
+		exit(1);
+	}
+
+	while ((len = fread(buf, 1, BUFSIZE, myFile)))
+	{
+		buf[len] = 0;
+		my_hash_value = getHash(my_hash_value + buf);
+	}
+
+	//get my file size
+	fseek(myFile, 0, SEEK_END);
+	int my_file_size = ftell(myFile);
+	sprintf(buf, "%d", my_file_size);
+
+	printf("receive_file_size : %s, my_file_size : %s\n", receive_file_size.c_str(), buf);
+	fclose(myFile);
+
+
+	//compare receive_hash_value, my_hash_value
+	if (receive_hash_value == my_hash_value)
+		printf("The file was successfully received.");
+	else
+		printf("The file download failed... Please try again");
 }
