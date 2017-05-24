b8740c4f284889a449bada2c5b76f8b392c6578f
hpk23 <hpk23@naver.com>
2017-05-24 17:24:37 +0900

    디렉토리 전송 UDP에 적용 (#10)

--- UdpServerSocket.h
+++ UdpServerSocket.h
@@ -10,7 +10,7 @@
 #include "md5.h"
 #pragma comment(lib, "ws2_32.lib")
 
-#define BUFSIZE 1024 * 30
+#define BUFSIZE 1024 * 10
 #define LISTENNUM 5
 
 using namespace std;
@@ -38,6 +38,7 @@ public :
 	string getHash(string md5Str);
 	void sendFile(char* file_name);
 	char* receiveMessage();
+	void searchFiles(char* path);
 };
 
 UdpServerSocket::UdpServerSocket(int port)
@@ -89,7 +90,7 @@ void UdpServerSocket::sendMessage(char* message)
 	int mLen = strlen(message);
 	strcpy(buf, message);
 	sendto(servSock, buf, mLen, 0, (struct sockaddr *)&cliAddr, sizeof(cliAddr));
-	Sleep(10);
+	Sleep(20);
 }
 
 string UdpServerSocket::getHash(string md5Str)
@@ -109,7 +110,8 @@ string UdpServerSocket::getHash(string md5Str)
 
 void UdpServerSocket::sendFile(char* file_name)
 {
-	printf("UDP protocol\n\n");
+	printf("\nUDP protocol\n");
+	printf("Transfer this file : %s\n", file_name);
 	string hash_value = "";
 	int len;
 	FILE *file;
@@ -142,4 +144,72 @@ void UdpServerSocket::sendFile(char* file_name)
 	//send hash_value
 	char* hash = (char*)hash_value.c_str();
 	sendMessage(hash);
+}
+
+void UdpServerSocket::searchFiles(char* path)
+{
+	DIR *dp;
+	struct dirent *dent;
+	char temp[1024];
+	char send_file_name[BUFSIZE] = "";
+
+	if((dp = opendir(path)) == NULL)
+	{ 
+		char temp_path[1024];
+		strcpy(temp_path, path);
+		char* ptr = strtok(temp_path, "/");
+		while(ptr != NULL)
+		{
+			if(!strcmp(ptr, "data"))
+			{
+				ptr = strtok(NULL, "/");
+				break;
+			}
+			ptr = strtok(NULL, "/");
+		}
+
+		while(ptr != NULL)
+		{
+			strcat(send_file_name, ptr);
+			ptr = strtok(NULL, "/");
+		}
+
+		sendMessage(send_file_name);
+		sendFile(path);
+	}
+
+	while((dent = readdir(dp)))
+	{
+		if(dent->d_name[0] == '.') continue;
+
+		sprintf(temp, "%s/%s", path, dent->d_name);
+		if(opendir(temp) != NULL) searchFiles(temp);
+
+		else
+		{
+			char temp_path[1024];
+			strcpy(temp_path, path);
+			char* ptr = strtok(temp_path, "/");
+			while(ptr != NULL)
+			{
+				if(!strcmp(ptr, "data"))
+				{
+					ptr = strtok(NULL, "/");
+					break;
+				}
+				ptr = strtok(NULL, "/");
+			}
+
+			while(ptr != NULL)
+			{
+				strcat(send_file_name, ptr);
+				ptr = strtok(NULL, "/");
+			}
+			strcat(send_file_name, "/");
+			strcat(send_file_name, dent->d_name);
+			sendMessage(send_file_name);
+			memset(send_file_name, 0, sizeof(send_file_name));
+			sendFile(temp);
+		} 
+	}
 }
