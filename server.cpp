50a25de4f6ddc4b4afd2615e2323891629df9e3c
hpk23 <hpk23@naver.com>
2017-05-24 17:13:32 +0900

    V0.2 TCP전송/무결성추가 최종 마무리

--- server.cpp
+++ server.cpp
@@ -23,21 +23,4 @@ int main()
 	tcp.sendFile(buf);
 
 	return 0;
-	/*char* message;
-	char buf[BUFSIZE];
-
-	UdpServerSocket udp(PORT);
-	udp.createSocket();
-	udp.bindSocket();
-
-	message = udp.receiveMessage();
-	printf("%s\n", message);
-
-	strcpy(buf, "client/step3.txt"); // 파일이름 복사
-	udp.sendMessage(buf); // 파일이름 클라이언트에게 보내기
-
-	strcpy(buf, "D:/S.W_3team/step3.txt"); //보낼 파일 경로 buf에 복사
-	udp.sendFile(buf); // 파일 보내기
-
-	return 0;*/
 }
